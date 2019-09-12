eb68fda70e086d81cb6403749498ffa804a6fced
Keith Cirkel <keithamus@users.noreply.github.com>
2019-09-12 11:29:47 +0100

    types(ts): add window declaration

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,9 @@
 export default class TextExpanderElement extends HTMLElement {
   readonly keys: Array<string>;
 }
+
+declare global {
+  interface Window {
+    TextExpanderElement: typeof TextExpanderElement
+  }
+}
