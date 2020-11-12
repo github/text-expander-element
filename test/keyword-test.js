712788c0f9f808a83942a7be8fcff25f51a72861
Yehor Lvivski <lvivski@github.com>
2020-11-12 20:15:33 +0100

    Initial multiword implementation

--- test/keyword-test.js
+++ test/keyword-test.js
@@ -1,58 +1,65 @@
-import keyword from '../dist/keyword'
+import query from '../dist/query'
 
-describe('text-expander keyword parsing', function() {
+describe('text-expander single word parsing', function() {
   it('does not match empty text', function() {
-    const found = keyword('', ':', 0)
+    const found = query('', ':', 0)
     assert(found == null)
   })
 
   it('does not match without activation key', function() {
-    const found = keyword('cat', ':', 3)
+    const found = query('cat', ':', 3)
     assert(found == null)
   })
 
   it('matches only activation key', function() {
-    const found = keyword(':', ':', 1)
-    assert.deepEqual(found, {word: '', position: 1})
+    const found = query(':', ':', 1)
+    assert.deepEqual(found, {text: '', position: 1})
   })
 
   it('matches trailing activation key', function() {
-    const found = keyword('hi :', ':', 4)
-    assert.deepEqual(found, {word: '', position: 4})
+    const found = query('hi :', ':', 4)
+    assert.deepEqual(found, {text: '', position: 4})
   })
 
   it('matches start of text', function() {
-    const found = keyword(':cat', ':', 4)
-    assert.deepEqual(found, {word: 'cat', position: 1})
+    const found = query(':cat', ':', 4)
+    assert.deepEqual(found, {text: 'cat', position: 1})
   })
 
   it('matches end of text', function() {
-    const found = keyword('hi :cat', ':', 7)
-    assert.deepEqual(found, {word: 'cat', position: 4})
+    const found = query('hi :cat', ':', 7)
+    assert.deepEqual(found, {text: 'cat', position: 4})
   })
 
   it('matches middle of text', function() {
-    const found = keyword('hi :cat bye', ':', 7)
-    assert.deepEqual(found, {word: 'cat', position: 4})
+    const found = query('hi :cat bye', ':', 7)
+    assert.deepEqual(found, {text: 'cat', position: 4})
   })
 
   it('matches only at word boundary', function() {
-    const found = keyword('hi:cat', ':', 6)
+    const found = query('hi:cat', ':', 6)
     assert(found == null)
   })
 
   it('matches last activation key word', function() {
-    const found = keyword('hi :cat bye :dog', ':', 16)
-    assert.deepEqual(found, {word: 'dog', position: 13})
+    const found = query('hi :cat bye :dog', ':', 16)
+    assert.deepEqual(found, {text: 'dog', position: 13})
   })
 
   it('matches closest activation key word', function() {
-    const found = keyword('hi :cat bye :dog', ':', 7)
-    assert.deepEqual(found, {word: 'cat', position: 4})
+    const found = query('hi :cat bye :dog', ':', 7)
+    assert.deepEqual(found, {text: 'cat', position: 4})
   })
 
   it('does not match with a space between cursor and activation key', function() {
-    const found = keyword('hi :cat bye', ':', 11)
+    const found = query('hi :cat bye', ':', 11)
     assert(found == null)
+  })
+})
+
+describe('text-expander multi word parsing', function() {
+  it('does matches with a space between cursor and activation key', function() {
+    const found = query('hi :cat bye', ':', 11, true)
+    assert.deepEqual(found, {text: 'cat bye', position: 4})
   })
 })
