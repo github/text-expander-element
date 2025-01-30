6df42075f8b9fcd461d8f1e9dc1d84bae4de7768
silverwind <me@silverwind.io>
2025-01-30 17:48:33 +0100

    Re-export the types in index.d.ts (#75)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import TextExpanderElement from './text-expander-element'
 export {TextExpanderElement as default}
+export type * from './text-expander-element'
 
 declare global {
   interface Window {
